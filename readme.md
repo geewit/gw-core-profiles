463e345367b7d9872a2b97e0c43249c914f22a15
fang.huang <fang.huang@guanaitong.com>
2018-09-06 10:11:40 +0800

    init

---
+++ readme.md
@@ -0,0 +1,1 @@
+Pre-Customized Spring Profiles
---
+++ src/main/java/io/geewit/core/Profiles.java
@@ -0,0 +1,14 @@
+package io.geewit.core;
+
+
+/**
+ * 环境常量
+ * @author geewit
+ */
+@SuppressWarnings({"unused"})
+public interface Profiles {
+    String DEVELOPMENT = "development";
+    String INTEGRATION = "integration";
+    String PREVIEW = "preview";
+    String PRODUCTION = "production";
+}
