c4f60fb4d6d56e9db7ba8059141b6064540e10f9
geewit <hf@geewit.io>
2019-06-19 09:20:13 +0800

    init

--- src/main/java/io/geewit/core/Profiles.java
+++ src/main/java/io/geewit/core/Profiles.java
@@ -1,15 +1,20 @@
 package io.geewit.core;
 
 
-
 /**
  * 环境常量
  * @author geewit
  */
 @SuppressWarnings({"unused"})
-public interface Profiles {
+public interface Profiles extends org.springframework.core.env.Profiles {
     String DEVELOPMENT = "development";
     String INTEGRATION = "integration";
     String PREVIEW = "preview";
     String PRODUCTION = "production";
+
+    org.springframework.core.env.Profiles PROFILES_DEVELOPMENT = org.springframework.core.env.Profiles.of(DEVELOPMENT);
+    org.springframework.core.env.Profiles PROFILES_INTEGRATION = org.springframework.core.env.Profiles.of(INTEGRATION);
+    org.springframework.core.env.Profiles PROFILES_PREVIEW = org.springframework.core.env.Profiles.of(PREVIEW);
+    org.springframework.core.env.Profiles PROFILES_PRODUCTION = org.springframework.core.env.Profiles.of(PRODUCTION);
+
 }
